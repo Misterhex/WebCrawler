e71f06fe11aa3617d86b31e78755a8cf80439db4
Yong Heng <Yongheng@live.com.sg>
2013-02-03 23:56:33 +0800

    first commit

---
+++ Example/Program.cs
@@ -0,0 +1,27 @@
+﻿using MisterHex.WebCrawling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IObservable<Uri> observable = Crawler.Crawl(new Uri("http://www.codinghorror.com/"));
+            observable.Subscribe(onNext: uri =>
+            {
+                Console.WriteLine(uri);
+            }
+            , onCompleted: () => Console.WriteLine("Crawling completed")
+            );
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
